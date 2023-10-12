f98b3fd963da134262c1419a5428141b6318e71d
Divak Maheshwari <divak.m14@gmail.com>
2023-10-12 16:22:26 -0700

    Add files via upload

--- merge.py
+++ merge.py
@@ -1,18 +1,15 @@
-def merge_list(input_1, input_2):
-    for x in input_2:
+def merge_list(input_1, list2):
+    for x in list2:
         input_1.append(x)
     return sort(input_1)
 #Selection Sort
 def sort(unsorted):
-    try:
         for i in range(len(unsorted)):
             if not isinstance(i, int):
-                raise TypeError
+                raise TypeError("TypeError")
             min_value = i;
             for j in range(i + 1, len(unsorted)):
                 if unsorted[j] <= unsorted[min_value]:
                     min_value = j;
             unsorted[i], unsorted[min_value] = unsorted[min_value], unsorted[i] 
         return unsorted
-    except TypeError as e:
-        return "TypeError"
