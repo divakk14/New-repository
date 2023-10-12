9718e35bcbb0f172e4f264d61f6e953d5494b992
Divak Maheshwari <divak.m14@gmail.com>
2023-10-12 15:25:51 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-from print_caps import allcaps
+# from print_caps import allcaps
 
-@allcaps
-def greet():
-    return "hello World!"
+# @allcaps
+# def greet():
+#     return "hello World!"
 
-def main():
-    print(greet())
+# def main():
+#     print(greet())
 
-main()
-from log import timestamp
+# main()
+# from log import timestamp
 
-@timestamp
-def hi():
-    print('hi')
+# @timestamp
+# def hi():
+#     print('hi')
 
-def main(): 
-    hi()
+# def main(): 
+#     hi()
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
