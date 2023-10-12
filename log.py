b0b53544f11d1357101d2f7fb028e90b66bb54d7
Divak Maheshwari <divak.m14@gmail.com>
2023-10-12 15:11:19 -0700

    Add files via upload

---
+++ log.py
@@ -0,0 +1,7 @@
+import time
+
+def timestamp(func):
+    def wrapper(*args, **kwargs):
+        print(time.ctime())
+        return func(*args, **kwargs)
+    return wrapper
--- test.py
+++ test.py
@@ -8,3 +8,14 @@ def main():
     print(greet())
 
 main()
+from log import timestamp
+
+@timestamp
+def hi():
+    print('hi')
+
+def main(): 
+    hi()
+
+if __name__ == '__main__':
+    main()
