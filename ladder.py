da77a8607e33f1d6eca4e4587ce16542568118e4
Divak Maheshwari <divak.m14@gmail.com>
2023-10-12 17:25:53 -0700

    Add files via upload

--- ladder.py
+++ ladder.py
@@ -1,20 +1,20 @@
-def my_steps(number):
+def my_steps(n):
     if not (1 <= n <= 25):
         raise ValueError("Input number must be between 1 and 25, inclusive.")
     
-    if number <= 2:
-        return number
+    if n <= 2:
+        return n
     else:
         # Init list - store the n of ways for every step
-        ways = [0] * (number + 1)
+        ways = [0] * (n + 1)
         ways[1] = 1  #1 way to climb a 1-step ladder 
         ways[2] = 2  #2 ways to climb a 2-step ladder
         
         # Calculate n of ways - each step
-        for i in range(3, number + 1):
+        for i in range(3, n + 1):
             ways[i] = ways[i - 1] + ways[i - 2]
         
-        return ways[number]
+        return ways[n]
 
 # # For testing
 # if __name__ == "__main__":
