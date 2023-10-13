fd29e834b57562e35985e333e549fa54382c3e57
Divak Maheshwari <divak.m14@gmail.com>
2023-10-12 17:29:20 -0700

    Add files via upload

--- sorted.py
+++ sorted.py
@@ -7,19 +7,16 @@ def reverse_sort_dictionary(input_dict):
     sorted_dict = dict(sorted(input_dict.items(), key=lambda item: item[0], reverse=True))
 
     # Create a list of tuples with names and phone numbers
-    result = [(name, data[0]) for name, data in sorted_dict.items()]
+    res = [(name, data[0]) for name, data in sorted_dict.items()]
 
-    return result
+    return res
 
 # # Sample input dictionary
-sample_input = {
-    'Tom': (5464512, 24),'Sara': (5446987, 32),'Mary': (1557896, 20)
-}
+# sample_input = {
+#     'Tom': (5464512, 24),'Sara': (5446987, 32),'Mary': (1557896, 20)
+# }
 
-# Call the function with the sample input
-result = reverse_sort_dictionary(sample_input)
+# # Call the function with the sample input
+# res = reverse_sort_dictionary(sample_input)
 
-# Print the result
-print(result)
-#output 
-#[('Tom', 5464512), ('Sara', 5446987), ('Mary', 1557896)]
+# print(res)
